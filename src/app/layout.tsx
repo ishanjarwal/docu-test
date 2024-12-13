ce5e1b456a908447a00baedc36a1bb4c05ea0a77
ishanjarwal <ishan.professional01@gmail.com>
2024-12-13 15:32:57 +0530

    final starter code with most configurations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({subsets:["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template:"%s | ResumeBuildr - Your AI Resume Builder",
-    absolute:"ResumeBuildr - Your AI Resume Builder"
+    template: "%s | ResumeBuildr - Your AI Resume Builder",
+    absolute: "ResumeBuildr - Your AI Resume Builder",
   },
-  description: "ResumeBuildr – Create professional, ATS-friendly resumes in minutes with AI-powered tools. Personalize templates, optimize keywords, and stand out to land your dream job. Start building today!",
+  description:
+    "ResumeBuildr – Create professional, ATS-friendly resumes in minutes with AI-powered tools. Personalize templates, optimize keywords, and stand out to land your dream job. Start building today!",
 };
 
 export default function RootLayout({
@@ -19,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={inter.className}
-      >
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main>
-      Home page
-    </main>
-  );
+  return <main>Home page</main>;
 }
